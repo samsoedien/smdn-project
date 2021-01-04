604340cc956af61e2a8236960e5f3a670040ed33
Nathan Samsoedien <contact@samsoedien.com>
2021-01-04 22:10:41 +0100

    chore: add bootstrap & bootstrap-icons

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -10,7 +10,12 @@ export interface IAppState {
 export interface IAppProps {}
 
 const App: React.FC<IAppProps> = () => {
-  return <div className={styles['App']}>React App</div>
+  return (
+    <div className={styles['App']}>
+      <h1>React App</h1>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  )
 }
 
 App.propTypes = {}
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
 import App from './app'
+import * as serviceWorker from './serviceWorker'
 import reportWebVitals from './reportWebVitals'
+
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +14,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
