68b993f9bb348910f7cf89170338ed1e06b99eaa
Nathan Samsoedien <info@samsoedien.com>
2021-01-16 20:07:44 +0100

    feat: add mongodb database solution

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,12 +2,16 @@ import http from 'http'
 import dotenv from 'dotenv'
 
 import app from './api/app'
+import connectDB from './config/mongoDB'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-  console.info(`Server running in ${process.env.NODE_ENV} mode at http://localhost:${PORT}/api`)
-}).on('error', console.error)
+server
+  .listen(PORT, () => {
+    connectDB()
+    console.info(`Server running at http://localhost:${PORT}`)
+  })
+  .on('error', console.error)
