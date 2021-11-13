6d6d7ad5d71ff84b773e4cb966ce6c08b0843dcd
Nathan Samsoedien <info@samsoedien.com>
2021-11-13 23:06:44 +0100

    chore: configure additional next features in client

---
+++ packages/client/src/pages/_middleware.tsx
@@ -0,0 +1,6 @@
+import type { NextFetchEvent, NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  // return new Response('Hello, world!')
+  return console.log('Hello, world!')
+}
