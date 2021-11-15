671d9dfc8d57bd24da4cb2455b5a7ccd518ee5bc
Nathan Samsoedien <info@samsoedien.com>
2021-11-15 19:29:42 +0100

    refactor: finetune page document

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -1,12 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
   render() {
     return (
-      <Html>
-        <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet" />
-        </Head>
+      <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -7,6 +7,9 @@ import Layout from '../layouts/layout'
 
 import styles from '../styles/Home.module.css'
 
+// The environment variable will be exposed to the browser
+console.info('My Application Version', process.env.NEXT_PUBLIC_VERSION)
+
 const HomePage = () => (
   <div className={styles.container}>
     <Head>
